22f87a2c80ea5d0b0004c605bc37dc7bb90e5f51
ggaleanoguerra <ggaleanoguerra@correo.unicordoba.edu.co>
2024-07-19 20:17:45 -0500

    refactor: Update NotFound.vue to use white text color for error message

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,8 +1,8 @@
 <template>
     <div>
-      <h1>404 - Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
-      <router-link to="/">Go to Home</router-link>
+      <h1 class="text-white">404 - Not Found</h1>
+      <p class="text-white">The page you are looking for does not exist. En pocas palabras, no sea sapo</p>
+      <router-link to="/" class="text-white">Go to Home</router-link>
     </div>
   </template>
   
