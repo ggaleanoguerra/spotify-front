681efd3849c856d61166928f5272cbe4b43a66eb
ggaleanoguerra <ggaleanoguerra@correo.unicordoba.edu.co>
2024-08-12 09:56:53 -0500

    refactor: Update authentication logic in auth.js to handle token expiry

--- src/store/auth.js
+++ src/store/auth.js
@@ -13,11 +13,22 @@ const setUserProfile = (profile) => {
 
 const checkAuth = () => {
   const token = localStorage.getItem('spotify_token');
-  if (token) {
-    isAuthenticated.value = true;
-    const profile = localStorage.getItem('spotify_user_profile');
-    if (profile) {
-      userProfile.value = JSON.parse(profile);
+  const tokenExpiry = localStorage.getItem('spotify_token_expiry'); // Assuming you store the token expiry time in localStorage
+
+  if (token && tokenExpiry) {
+    const now = new Date().getTime();
+    if (now < tokenExpiry) {
+      isAuthenticated.value = true;
+      const profile = localStorage.getItem('spotify_user_profile');
+      if (profile) {
+        userProfile.value = JSON.parse(profile);
+      }
+    } else {
+      // Token has expired
+      localStorage.removeItem('spotify_token');
+      localStorage.removeItem('spotify_token_expiry');
+      isAuthenticated.value = false;
+      userProfile.value = null;
     }
   } else {
     isAuthenticated.value = false;
--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,16 +1,14 @@
 <template>
     <div>
       <h1 class="text-white">404 - Not Found</h1>
-      <p class="text-white">The page you are looking for does not exist. En pocas palabras, no sea sapo</p>
+      <p class="text-white">The page you are looking for does not exist.</p>
       <router-link to="/" class="text-white">Go to Home.</router-link>
     </div>
   </template>
   
   <script setup>
-  // Puedes agregar lógica adicional si es necesario
   </script>
   
   <style scoped>
-  /* Estilos personalizados */
   </style>
   
